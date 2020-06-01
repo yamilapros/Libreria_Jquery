d34c40f2dffb9fdd4413c9ca1bfa42d9723eec68
yamilapros <yamiprosdocimo@gmail.com>
2020-06-01 11:35:22 +0200

    Cambios en clase active en tabs

--- tabs_jquery/js/js.js
+++ tabs_jquery/js/js.js
@@ -5,20 +5,26 @@ $(document).ready(function(){
     
 
     function tabs(){
+        //El primer item que tenga la clase active
+        $('.tabs-nav li a:first').addClass('active');
         //Primero Ocultar todos los div de contenido
         $('.contenido').hide();
         //Mostrar primer div
         $('.contenido:first').show();
-        $('.tabs-nav li').click(function(){
-            //Remuevo la clase active en los que la tengan y la activo en este, el seleccionado
-            $('.tabs-nav li').removeClass('active');
+
+        $('.tabs-nav li a').click(function(){
+            //Remuevo la clase active en los que la tengan y la activo en este, el seleccionado3
+            $('.tabs-nav li a').removeClass('active');
             $(this).addClass('active');
+
             //Oculto todos los contenidos 
             $('.contenido').hide();
             //Tengo que sacar el href del li que se haya seleccionado 
-            let contenidoActivo = $(this).find('a').attr('href');
-            $(contenidoActivo).fadeIn(1000);
+            let contenidoActivo = $(this).attr('href');
+            $(contenidoActivo).fadeIn(400);
+            return false;
         })
+        
     }
     
 })
